837adb65ec60e4c5e9b6f2d5f9a730ddfc6ce76c
ch1lamLee <39334548+ch1lamLee@users.noreply.github.com>
2019-01-09 21:50:19 +0800

    顾客管理页面

--- storeWeb/src/views/home/userManager.vue
+++ storeWeb/src/views/home/userManager.vue
@@ -2,8 +2,8 @@
   <div>
     <el-main>
       <el-table :data="tableData" style="width: 100%">
-        <el-table-column prop="name" label="用户名" min-width="150"></el-table-column>
-        <el-table-column prop="rank" label="用户等级" min-width="150"></el-table-column>
+        <el-table-column prop="name" label="客户名字" min-width="150"></el-table-column>
+        <el-table-column prop="rank" label="客户等级" min-width="150"></el-table-column>
         <el-table-column min-width="100" align="right">
           <template slot="header" slot-scope="scope">
             <el-button size="mini" @click="handleAdd">Add</el-button>
@@ -18,22 +18,67 @@
           </template>
         </el-table-column>
       </el-table>
+      <!-- 编辑对话框 -->
+      <el-dialog title="客户信息" :visible.sync="editDialogVisible">
+        <el-form :model="editForm">
+          <el-form-item label="客户名字">
+            <el-input v-model="editForm.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="客户等级">
+            <el-input v-model="editForm.rank" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="editDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="editGuests()">确 定</el-button>
+        </div>
+      </el-dialog>
+
+      <!-- 新增对话框 -->
+      <el-dialog title="新增客户" :visible.sync="addDialogVisible">
+        <el-form :model="addForm">
+          <el-form-item label="客户名字">
+            <el-input v-model="addForm.name" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="客户等级">
+            <el-input v-model="addForm.rank" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="addDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="addGoods">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-main>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
+  // 注入刷新页面的依赖
+  inject: ["reload"],
   data() {
     return {
-      tableData: []
+      tableData: [],
+      editDialogVisible: false,
+      addDialogVisible: false,
+      editForm: {
+        name: "",
+        rank: "",
+        id:""
+      },
+      addForm: {
+        name: "",
+        rank: ""
+      }
     };
   },
   mounted() {
     axios
-      .get("http://localhost:8080/searchGuest", {
+      .get("http://localhost:8080/searchGuests", {
         params: {
           data: "",
-          type: "3"
+          rank: "3"
         }
       })
       .then(res => {
@@ -42,6 +87,38 @@ export default {
           this.tableData.push(element);
         });
       });
+  },
+  methods: {
+    handleAdd() {
+      this.addDialogVisible = true;
+    },
+    handleEdit(index, row) {
+      this.editForm.name = row.name;
+      this.editForm.rank = row.rank;
+      this.editForm.id=row.id;
+      this.editDialogVisible = true;
+    },
+    handleDelete(index, row) {
+      axios
+        .get("http://localhost:8080/deleteGuests", {
+          params: {
+            no: row.no
+          }
+        })
+        .then(this.reload());
+    },
+    editGuests() {
+      console.log(this.addForm);
+      axios
+        .get("http://localhost:8080/updateGuests", {
+          params: {
+            name: this.editForm.name,
+            rank: this.editForm.rank,
+            id: this.editForm.id
+          }
+        })
+        .then(this.reload());
+    }
   }
 };
 </script>
